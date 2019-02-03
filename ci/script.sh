f2b9898b5edf0f25443f22061bf07657008afc07
Pika <pika@thom.guide>
2019-02-03 13:35:16 -0500

    Revert "add clippy to ci"
    This reverts commit 26cd0a901381ecf4edf01193ccae0fe51cd96e1f.

--- ci/script.sh
+++ ci/script.sh
@@ -14,9 +14,6 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross clippy --target $TARGET
-    cross clippy --target $TARGET --release
-
     cross run --target $TARGET
     cross run --target $TARGET --release
 }
